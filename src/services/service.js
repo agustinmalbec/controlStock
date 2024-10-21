2b077987abae37f1077425422cba49ddb35406c2
Francisco Agustin <agustinmalbec123@gmail.com>
2024-10-21 15:35:53 -0300

    Prueba

--- src/services/service.js
+++ src/services/service.js
@@ -2,7 +2,7 @@ import ChangesService from "./dao/mongodb/changes.service.js";
 import ItemService from "./dao/mongodb/items.service.js";
 import ChangesRepository from "./repository/changes.repository.js";
 import ItemRepository from "./repository/items.repository.js";
-import UserService from "./dao/mongoDb/users.service.js";
+import UserService from "./dao/mongodb/users.service.js";
 import UserRepository from "./repository/users.repository.js";
 
 const itemDAO = new ItemService();
